882d272ab9a6f67b41afc9287f240447c491c427
codewarrior0506 <amarjeet9781@gmail.com>
2025-05-06 16:43:08 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let input = document.querySelector('#input')
+let btnGenerateQR = document.querySelector('#generateQR')
+let imageQR = document.querySelector('#imageQR')
+
+function generateQR(){
+    if (input.value === '') {
+        alert('please provide Valid Input')
+        return
+    }
+
+    imageQR.setAttribute('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + input.value )
+}
+
+btnGenerateQR.addEventListener('click', generateQR)
